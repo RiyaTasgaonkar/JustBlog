67c1075f8e6f282f4a0fc231da0e17f6387bb5c0
RiyaTasgaonkar <riyatasgaonkar@gmail.com>
2020-11-18 20:29:40 +0530

    blog opertaions updated

--- blogging/blog_app/urls.py
+++ blogging/blog_app/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path,include
 from django.contrib.auth import views as authViews
 from . import views
+from .views import detail_view,upload,update,delete_view
 
 urlpatterns = [
     path('',views.home, name='home'),
     path('posts/',views.posts, name='posts'),
+    path('myposts/',views.my_posts,name='my_posts'),
+    path('post/<int:pk>',detail_view.as_view(),name='post-details'),
+    path('upload/',upload.as_view(),name='upload'),
+    path('post/<int:pk>/update',update.as_view(),name='update'),
+    path('post/<int:pk>/delete',delete_view.as_view(),name='delete'),
     path('register/',views.register,name='register'),
     path('login/',authViews.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/',views.logout_view,name='logout'),
--- blogging/blog_app/views.py
+++ blogging/blog_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render,redirect
+from django.utils import timezone
 from django.http import HttpResponse
 from .models import Post
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
@@ -26,11 +30,61 @@ def register(request):
 
 @login_required
 def posts(request): 
-    posts=Post.objects.all()
+    posts=Post.objects.all().order_by('-date')
     user=request.user.username
     return render(request,'posts.html', {'posts':posts,'user':user})
 
+@login_required
+def my_posts(request):
+    u=request.user.username
+    posts=Post.objects.filter(postAuthor__exact=request.user.id).order_by('-date')
+    return render(request,'my_posts.html', {'posts':posts,'user':u})
+
+@login_required
 def logout_view(request):
     logout(request)
     messages.info(request,'You have been logged out.')
     return redirect('home')
+
+class detail_view(LoginRequiredMixin, DetailView):
+    model=Post
+    template_name='post-details.html'
+
+class upload(LoginRequiredMixin,CreateView):
+    model=Post
+    template_name='upload.html'
+    fields=['postTitle','postContent']
+    def form_valid(self, form):
+        form.instance.postAuthor=self.request.user
+        return super().form_valid(form)
+        
+    def get_success_url(self):
+        return reverse('my_posts')
+
+class update(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model=Post
+    template_name='upload.html'
+    fields=['postTitle','postContent']
+    def form_valid(self, form):
+        form.instance.postAuthor=self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        p=self.get_object()
+        return p.postAuthor==self.request.user
+    
+    def get_success_url(self):
+        messages.info(self.request,'Post has been updated.')
+        return reverse('my_posts')
+ 
+class delete_view(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name='delete.html'
+
+    def test_func(self):
+        p=self.get_object()
+        return p.postAuthor==self.request.user
+
+    def get_success_url(self):
+        messages.info(self.request,'Post has been deleted.')
+        return reverse('my_posts')
